86c393a8d797046cba3aaf76fe0c9dd4c9a26a73
nexcodes <mohidnaeem41@gmail.com>
2023-11-29 00:52:19 +0500

    resolve errors

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,71 +1,71 @@
-import { cookies, headers } from "next/headers";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import Stripe from "stripe";
+// import { cookies, headers } from "next/headers";
+// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+// import Stripe from "stripe";
 
-import { Database } from "@/types/db";
-import { stripe } from "@/lib/stripe";
+// import { Database } from "@/types/db";
+// import { stripe } from "@/lib/stripe";
 
-export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
-  const supabase = createRouteHandlerClient<Database>({
-    cookies,
-  });
+// export async function POST(req: Request) {
+//   const body = await req.text();
+//   const signature = headers().get("Stripe-Signature") as string;
+//   const supabase = createRouteHandlerClient<Database>({
+//     cookies,
+//   });
 
-  let event: Stripe.Event;
+//   let event: Stripe.Event;
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    );
-  } catch (error: any) {
-    return new Response(`Webhook Error: ${error.message}`, { status: 400 });
-  }
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET!
+//     );
+//   } catch (error: any) {
+//     return new Response(`Webhook Error: ${error.message}`, { status: 400 });
+//   }
 
-  const session = event.data.object as Stripe.Checkout.Session;
+//   const session = event.data.object as Stripe.Checkout.Session;
 
-  if (event.type === "checkout.session.completed") {
-    // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    );
+//   if (event.type === "checkout.session.completed") {
+//     // Retrieve the subscription details from Stripe.
+//     const subscription = await stripe.subscriptions.retrieve(
+//       session.subscription as string
+//     );
 
-    // Update the user stripe into in our database.
-    // Since this is the initial subscription, we need to update
-    // the subscription id and customer id.
+//     // Update the user stripe into in our database.
+//     // Since this is the initial subscription, we need to update
+//     // the subscription id and customer id.
 
-    await supabase
-      .from("users")
-      .update({
-        stripe_customer_id: subscription.id,
-        stripe_subscription_id: subscription.customer as string,
-        stripe_price_id: subscription.items.data[0].price.id,
-        stripe_current_period_end: new Date(
-          subscription.current_period_end * 1000
-        ).toISOString(),
-      })
-      .eq("email", session?.customer_email ?? "");
-  }
+//     await supabase
+//       .from("users")
+//       .update({
+//         stripe_customer_id: subscription.id,
+//         stripe_subscription_id: subscription.customer as string,
+//         stripe_price_id: subscription.items.data[0].price.id,
+//         stripe_current_period_end: new Date(
+//           subscription.current_period_end * 1000
+//         ).toISOString(),
+//       })
+//       .eq("email", session?.customer_email ?? "");
+//   }
 
-  if (event.type === "invoice.payment_succeeded") {
-    // Retrieve the subscription details from Stripe.
-    const subscription = await stripe.subscriptions.retrieve(
-      session.subscription as string
-    );
+//   if (event.type === "invoice.payment_succeeded") {
+//     // Retrieve the subscription details from Stripe.
+//     const subscription = await stripe.subscriptions.retrieve(
+//       session.subscription as string
+//     );
 
-    // Update the price id and set the new period end.
-    await supabase
-      .from("users")
-      .update({
-        stripe_price_id: subscription.items.data[0].price.id,
-        stripe_current_period_end: new Date(
-          subscription.current_period_end * 1000
-        ).toISOString(),
-      })
-      .eq("email", session?.customer_email ?? "");
-  }
+//     // Update the price id and set the new period end.
+//     await supabase
+//       .from("users")
+//       .update({
+//         stripe_price_id: subscription.items.data[0].price.id,
+//         stripe_current_period_end: new Date(
+//           subscription.current_period_end * 1000
+//         ).toISOString(),
+//       })
+//       .eq("email", session?.customer_email ?? "");
+//   }
 
-  return new Response(null, { status: 200 });
-}
+//   return new Response(null, { status: 200 });
+// }
