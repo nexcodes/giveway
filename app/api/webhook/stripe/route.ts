9256b4f70cc322297f3be9e38cd4eb4ae7396ad2
nexcodes <mohidnaeem41@gmail.com>
2023-12-01 14:23:45 +0500

    nothing

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -6,6 +6,7 @@ import { Database } from "@/types/db";
 import { stripe } from "@/lib/stripe";
 
 export async function POST(req: Request) {
+  console.log(1);
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
   const supabase = createRouteHandlerClient<Database>({
@@ -24,14 +25,18 @@ export async function POST(req: Request) {
     return new Response(`Webhook Error: ${error.message}`, { status: 400 });
   }
 
+  console.log(2);
+
   const session = event.data.object as Stripe.Checkout.Session;
 
   if (event.type === "checkout.session.completed") {
+    console.log(3);
     // Retrieve the subscription details from Stripe.
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
 
+    console.log(4);
     const balance =
       process.env.STRIPE_PREMIUM_PLAN_PRICE_ID ===
       subscription.items.data[0].plan.id
@@ -44,6 +49,7 @@ export async function POST(req: Request) {
     // Since this is the initial subscription, we need to update
     // the subscription id and customer id.
 
+    console.log(5);
     await supabase
       .from("users")
       .update({
@@ -54,6 +60,7 @@ export async function POST(req: Request) {
         balance,
       })
       .eq("email", session?.customer_email ?? "");
+    console.log(6);
   }
 
   if (event.type === "invoice.payment_succeeded") {
